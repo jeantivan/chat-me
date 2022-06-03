efc6b8b35597db6bb756270fd1236707031bc880
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-02 20:54:33 -0400

    No se usa export default

--- src/components/GithubIcon.js
+++ src/components/GithubIcon.js
@@ -2,7 +2,7 @@
 //
 // <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
 
-export default function GithubIcon() {
+export function GithubIcon() {
   return (
     <span
       role="img"
