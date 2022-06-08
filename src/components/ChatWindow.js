5bc84bf87140c31e227d1785c9dbd02e263cf086
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-07 22:04:38 -0400

    Primer diseño de la ventana de Chat

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,7 +1,10 @@
+import { MinusIcon } from "./MinusIcon";
+import { SendIcon } from "./SendIcon";
+
 export function ChatWindow() {
   return (
     <aside
-      className="dark:bg-slate-700 xs:w-full w-96 rounded-lg overflow-hidden"
+      className="dark:bg-slate-700 xs:w-full w-96 rounded-lg overflow-hidden flex flex-col"
       style={{ height: 400 }}
     >
       <header className="bg-emerald-400 p-4 flex items-center">
@@ -10,11 +13,19 @@ export function ChatWindow() {
           John Doe
         </div>
         <div className="flex">
-          <button className="inline-flex items-center justify-center font-bold hover:bg-emerald-100 w-8 h-8 rounded-full">
-            -
+          <button className="inline-flex items-center justify-center font-bold hover:bg-emerald-100 w-8 h-8 py-2 rounded-full">
+            <MinusIcon />
           </button>
         </div>
       </header>
+      <div className="p-2 dark:bg-slate-900 w-full flex items-center mt-auto">
+        <div className="dark:bg-slate-700 flex-1  self-stretch px-2 p-1 flex items-center mr-2 rounded-xl">
+          <p className="text-slate-400">Escribe tu mensaje...</p>
+        </div>
+        <button className="inline-flex items-center justify-center font-bold text-emerald-200 hover:text-emerald-100 bg-emerald-800 hover:bg-emerald-700 w-10 h-10  rounded-full">
+          <SendIcon />
+        </button>
+      </div>
     </aside>
   );
 }
