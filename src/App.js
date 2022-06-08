b465daebbf51945277deea424a6241628b8d9029
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-07 22:21:14 -0400

    Contenerdor para controlar el cambio del modo oscuro

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { Header, Ball } from "./components";
+import { Header, Ball, ChatWindow } from "./components";
 import { useRef } from "react";
 
 function App() {
   const containerRef = useRef(null);
 
   return (
-    <>
+    <div class="dark app-container">
       <Header />
-      <main className="main flex p-4">
+      <main className="main flex p-4 justify-center">
+        <ChatWindow />
         <div ref={containerRef} className="w-full h-full relative">
           <Ball ref={containerRef} />
         </div>
       </main>
-    </>
+    </div>
   );
 }
 
