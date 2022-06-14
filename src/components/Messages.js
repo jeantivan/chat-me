4c346b6781e8f2f9e44c7f07521028f7b09fea75
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-13 21:26:33 -0400

    Diseño del componente contenedor de los mensajes del chat

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,3 +1,40 @@
-export function Messages({ messages }) {
-  return <div className="messages"></div>;
+import { useRef, useEffect } from "react";
+import { Message } from "./Message";
+
+export function Messages({ messages = [] }) {
+  const messagesContainer = useRef(null);
+
+  useEffect(() => {
+    if (messagesContainer.current) {
+      messagesContainer.current.scrollTo(
+        0,
+        messagesContainer.current.scrollHeight
+      );
+    }
+  }, []);
+
+  return (
+    <div
+      className="messages overflow-auto bg-neutral-200 dark:bg-slate-900"
+      ref={messagesContainer}
+    >
+      <div className="flex flex-col-reverse justify-start px-20 py-3">
+        {messages.map((message, i, array) => {
+          if (i === array.length - 1) {
+            console.log("Last item");
+            return (
+              <Message key={message.id} {...message} isFirstMessage={true} />
+            );
+          }
+          return (
+            <Message
+              key={message.id}
+              {...message}
+              isFirstMessage={message.type !== array[i + 1].type}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
