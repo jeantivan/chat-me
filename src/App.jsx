9608352246f7c094c524286ea101e57a2670a065
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-21 19:23:38 -0400

    Cambio el nombre de una prop en ContactInfo

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,50 @@ import {
   InputContainer,
   Messages,
   UserProfile,
+  Welcome,
 } from "./components";
 import "./App.css";
 import MESSAGES from "./utils/messages.json";
+import { CHATS } from "./utils/constants";
+
+const CONTACTS = [];
 
 function App() {
   const { mode } = useDarkMode();
   const [messages, setMessages] = useState([...MESSAGES]);
+  const [selectedChat, setSelectedChat] = useState({
+    id: "",
+    contactName: "",
+    lastMessage: "",
+    time: "",
+    photo: "",
+  });
+
+  console.log(selectedChat);
 
   return (
     <div className={`${mode} app-container`}>
-      <Header />
       <main className="main chat-container dark:bg-slate-900 overflow-hidden">
-        <ContactInfo />
         <UserProfile />
-        <ChatList />
-        <Messages messages={messages} />
-        <InputContainer setMessages={setMessages} />
+        <ChatList
+          selectedChat={selectedChat}
+          setSelectedChat={setSelectedChat}
+          contacts={CONTACTS}
+          chats={CHATS}
+        />
+
+        {!selectedChat.contactName ? (
+          <Welcome />
+        ) : (
+          <>
+            <ContactInfo
+              setSelectedChat={setSelectedChat}
+              selectedChat={selectedChat}
+            />
+            <Messages messages={messages} />
+            <InputContainer setMessages={setMessages} />
+          </>
+        )}
       </main>
     </div>
   );
