873572168ed12f72a5d08a95adc48e7e13342c94
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-21 19:23:11 -0400

    Nombre del contacto viene del chat selecionado

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,8 +1,8 @@
 import { FaUserCircle } from "react-icons/fa";
-import { BsThreeDotsVertical, BsSearch } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
 import { ContactMenu } from "./ContactMenu";
 
-export function ContactInfo({ contact }) {
+export function ContactInfo({ selectedChat, setSelectedChat }) {
   return (
     <div className="contact-info dark:bg-slate-800 bg-zinc-50 border-solid border-b border-slate-200 dark:border-slate-600">
       <div className="flex items-center px-5 py-2">
@@ -10,14 +10,16 @@ export function ContactInfo({ contact }) {
           <div className="text-gray-400 w-10 h-10 rounded-full mr-3">
             <FaUserCircle className="h-full w-full" />
           </div>
-          <p className="text-lg font-bold dark:text-white">Jane Doe</p>
+          <p className="text-lg font-bold dark:text-white">
+            {selectedChat.contactName}
+          </p>
         </div>
         <div className="flex items-center">
           <div className="h-9 w-9 ml-2 rounded-full dark:text-gray-400 text-gray-500 hover:bg-slate-200 dark:hover:bg-slate-600 p-2">
             <BsSearch className="h-full w-full" />
           </div>
           <div className="ml-2">
-            <ContactMenu />
+            <ContactMenu setSelectedChat={setSelectedChat} />
           </div>
         </div>
       </div>
