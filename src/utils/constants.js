0bb5ed084783f2723ec7e8ea3fcb77bbbdfffa48
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-26 18:06:28 -0400

    Key para el local storage de la información del usuario. Valor inicial del usuario.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,11 @@
 export const COLOR_MODE_KEY = "chatme-dark-mode";
+export const USER_PROFILE_KEY = "chatme-user";
+
+export const INITIAL_USER = {
+  name: "Jane Doe",
+  status: "Hola estoy usando ChatMe!",
+  picture: "",
+};
 
 export const INITIAL_CHAT = {
   id: "",
