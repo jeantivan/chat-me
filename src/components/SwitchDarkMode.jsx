0a3c6d610a4ee75033bf2760c80d51c86ced0a5b
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-01 22:30:00 -0400

    Usa el Switch comun del proyecto

--- src/components/SwitchDarkMode.jsx
+++ src/components/SwitchDarkMode.jsx
@@ -1,7 +1,6 @@
-import * as Switch from "@radix-ui/react-switch";
+import { Switch } from "./Switch";
 import { useDarkMode } from "./DarkMode";
 import { BsMoonStarsFill } from "react-icons/bs";
-import cx from "classnames";
 
 export function SwitchDarkMode() {
   const { mode, toggleDarkMode } = useDarkMode();
@@ -9,28 +8,13 @@ export function SwitchDarkMode() {
   return (
     <div className="dark:text-white flex w-full">
       <div className="flex items-center flex-1">Dark Mode</div>
-      <Switch.Root
-        className={cx(
-          "group",
-          "radix-state-checked:bg-emerald-500",
-          "radix-state-unchecked:bg-gray-400 dark:radix-state-unchecked:bg-gray-800",
-          "relative inline-flex h-[24px] w-[44px] flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out",
-          "focus:outline-none focus-visible:ring focus-visible:ring-emerald-500 focus-visible:ring-opacity-75"
-        )}
+
+      <Switch
         checked={mode === "dark"}
         onCheckedChange={() => toggleDarkMode()}
       >
-        <Switch.Thumb
-          className={cx(
-            "group-radix-state-checked:translate-x-5",
-            "group-radix-state-unchecked:translate-x-0",
-            "pointer-events-none inline-block h-[20px] w-[20px] p-1 transform rounded-full text-neutral-700 bg-white ring-0 transition duration-200 ease-in-out"
-          )}
-          asChild
-        >
-          <BsMoonStarsFill className="w-full h-full" />
-        </Switch.Thumb>
-      </Switch.Root>
+        <BsMoonStarsFill className="w-full h-full" />
+      </Switch>
     </div>
   );
 }
