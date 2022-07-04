aee981024d81be1ba8193ab8298b3667412f6d37
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-03 20:20:06 -0400

    Eliminada la pelotica

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { Header } from "./Header";
 export { Container } from "./Container";
-export { Ball } from "./Ball";
+//export { Ball } from "./Ball";
 export { ChatWindow } from "./ChatWindow";
 export { DarkModeProvider, useDarkMode, DarkModeContext } from "./DarkMode";
 export { ChatList } from "./ChatList";
