4a9960097fa4976badc324501913e4933724ed66
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-09 22:12:17 -0400

    Cambios en el layout para agregar la info de contacto

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import { ContactInfoContent, useContactInfo } from "./components/ContactInfo";
 
 function App() {
   const { mode } = useDarkMode();
-  const { openContactInfo } = useContactInfo();
+  const { openContactInfo, setOpenContactInfo } = useContactInfo();
   const [messages, setMessages] = useState([]);
   const [selectedChat, setSelectedChat] = useState(INITIAL_CHAT);
   const { isError, isLoading, data } = useGetContacts();
@@ -26,39 +26,43 @@ function App() {
     setMessages([]);
     if (selectedChat.id !== INITIAL_CHAT.id) {
       setTimeout(() => {
-        setMessages([...MESSAGES]);
+        setMessages([...MESSAGES.slice(0, 20)]);
       }, 250);
     }
   }, [selectedChat]);
 
   return (
-    <div className={`${mode} app-container md:overflow-x-auto`}>
-      <main className="main chat-container min-w-md dark:bg-slate-900 overflow-hidden">
-        <UserProfile />
-        <ChatList
-          selectedChat={selectedChat}
-          setSelectedChat={setSelectedChat}
-          chats={data}
-          isError={isError}
-          isLoading={isLoading}
-        />
-
-        {!selectedChat.name.fullName ? (
-          <Welcome />
-        ) : (
-          <>
-            <ChatHeader
-              setSelectedChat={setSelectedChat}
-              selectedChat={selectedChat}
-            />
-            <Messages messages={messages} />
-            <InputContainer setMessages={setMessages} />
-          </>
-        )}
-        <ContactInfoContent
-          selectedChat={selectedChat}
-          openContactInfo={openContactInfo}
-        />
+    <div
+      className={`${mode} md:overflow-x-auto max-h-screen min-w-screen flex`}
+    >
+      <main className="app-container min-w-md w-full dark:bg-slate-900 overflow-hidden flex">
+        <div className="user-chats">
+          <UserProfile />
+          <ChatList
+            selectedChat={selectedChat}
+            setSelectedChat={setSelectedChat}
+            chats={data}
+            isError={isError}
+            isLoading={isLoading}
+          />
+        </div>
+        <div className="flex-1 w-full flex">
+          <div className="chat-container flex-1">
+            {!selectedChat.name.fullName ? (
+              <Welcome />
+            ) : (
+              <>
+                <ChatHeader
+                  setSelectedChat={setSelectedChat}
+                  selectedChat={selectedChat}
+                />
+                <Messages messages={messages} />
+                <InputContainer setMessages={setMessages} />
+              </>
+            )}
+          </div>
+          <ContactInfoContent selectedChat={selectedChat} />
+        </div>
       </main>
     </div>
   );
