55e69f6f685a53d8266ded082f7f851bf6cb4a6e
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-09 22:28:56 -0400

    Abre info de contacto con click en header del chat

--- src/components/ChatHeader.jsx
+++ src/components/ChatHeader.jsx
@@ -1,25 +1,32 @@
 import { BsSearch } from "react-icons/bs";
 import { ContactMenu } from "./ContactMenu";
+import { useContactInfo } from "./ContactInfo";
 
 export function ChatHeader({ selectedChat, setSelectedChat }) {
+  const { setOpenContactInfo } = useContactInfo();
   const { name, picture } = selectedChat;
   return (
-    <div className="chat-header dark:bg-slate-800 bg-zinc-50 border-solid border-b border-slate-200 dark:border-slate-600">
+    <header className="chat-header dark:bg-slate-800 bg-zinc-50 border-solid border-b border-slate-200 dark:border-slate-600">
       <div className="flex items-center px-5 py-2">
-        <div className="flex-1 flex items-center">
-          <div className="text-gray-400 w-10 h-10 rounded-full mr-3">
+        <button
+          onClick={() => {
+            setOpenContactInfo(true);
+          }}
+          className="flex-1 flex items-stretch"
+        >
+          <span className="text-gray-400 w-10 h-10 rounded-full mr-3">
             <img
               className="bg-gray-400 w-full h-full rounded-full"
               src={picture.thumbnail}
               alt={`Foto de perfil de ${name.fullName}`}
             />
-          </div>
-          <div>
-            <p className="font-bold dark:text-white leading-none">
+          </span>
+          <span className="flex items-center">
+            <span className="font-bold dark:text-white leading-none">
               {name.fullName}
-            </p>
-          </div>
-        </div>
+            </span>
+          </span>
+        </button>
         <div className="flex items-center">
           <div className="h-9 w-9 ml-2 rounded-full dark:text-gray-400 text-gray-500 hover:bg-slate-200 dark:hover:bg-slate-600 p-2">
             <BsSearch className="h-full w-full" />
@@ -32,6 +39,6 @@ export function ChatHeader({ selectedChat, setSelectedChat }) {
           </div>
         </div>
       </div>
-    </div>
+    </header>
   );
 }
