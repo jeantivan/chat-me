0750d83311679c86a8b7d14bd7ef6fa7c054f1e9
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-11 19:33:23 -0400

    Border derecho

--- src/components/SearchChats.jsx
+++ src/components/SearchChats.jsx
@@ -2,7 +2,7 @@ import { BsSearch } from "react-icons/bs";
 import { CustomIcon } from "./CustomIcon";
 export function SearchChats() {
   return (
-    <div className="dark:bg-slate-800 bg-white p-2 border-b border-slate-200 dark:border-slate-600 border-solid">
+    <div className="dark:bg-slate-800 bg-white p-2 border-b border-r border-slate-200 dark:border-slate-600 border-solid">
       <div className="flex dark:text-gray-400 text-gray-500 dark:bg-slate-700 bg-zinc-100 rounded-md">
         <CustomIcon label="Buscar" Icon={BsSearch} className="w-fit p-2 pl-3" />
         <div className="w-full">
