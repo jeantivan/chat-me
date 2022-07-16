8d5dd8c83416496b82b82378dbba1d7ac3bec05c
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-16 18:44:38 -0400

    Comenzando migración a Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import {
   DarkModeProvider,
   UserInfoProvider,
@@ -14,7 +13,9 @@ import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
