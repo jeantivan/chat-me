80407b89bbec0da264b0274be818d2b3c10418e9
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-18 20:12:15 -0400

    Renombrados algunos tipos y creados algunos nuevos

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,12 @@
-export type User = {
+import { Dispatch, SetStateAction } from "react";
+
+export type UserType = {
   name: string;
   status: string;
   picture: string;
 };
 
-export type Contact = {
+export type ContactType = {
   name: { title: string; first: string; last: string; fullName: string };
   phone: string;
   picture: {
@@ -12,22 +14,33 @@ export type Contact = {
     medium: string;
     thumbnail: string;
   };
-  lastMessage?: Message;
+  lastMessage: MessageType;
 };
 
-export type Message = {
+export type MessageType = {
   id: string;
-  type: "send" | "received";
-  status: "sended" | "received" | "read";
+  isOwnMsg: boolean;
+  status: "send" | "received" | "read";
   message: {
-    type: "audio" | "video" | "image" | "text" | "sticker";
+    type: "audio" | "video" | "image" | "text";
     content: string;
   };
-  time: Date | string;
-  reactions?: Array<Reaction>;
+  time: string;
+  reactions: Array<ReactionType> | null;
 };
 
-export type Reaction = {
+export type ReactionType = {
   emoji: string;
   title: string;
+};
+
+export type SelectedChatState = {
+  selectedChat: ContactType;
+  setSelectedChat: Dispatch<SetStateAction<ContactType | null>>;
+};
+
+export type ShortCut = {
+  slug: string;
+  title: string;
+  keys: string[];
 };
