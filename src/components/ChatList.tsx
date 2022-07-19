96034283aeda4cf31b3a344dfaf4bfbe87f24a80
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-18 20:52:04 -0400

    Tipado de las props del componente

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,4 +1,11 @@
+import { ContactType, SelectedChatState } from "../types";
 import { ChatItem } from "./ChatItem";
+
+interface ChatListProps extends SelectedChatState {
+  chats?: ContactType[];
+  isError: boolean;
+  isLoading: boolean;
+}
 
 export function ChatList({
   chats,
@@ -6,7 +13,7 @@ export function ChatList({
   setSelectedChat,
   isError,
   isLoading,
-}) {
+}: ChatListProps) {
   return (
     <div className="chats-list h-full overflow-x-hidden dark:bg-slate-800/80 border-solid border-r border-slate-200 dark:border-slate-600 flex flex-col">
       <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
@@ -22,7 +29,7 @@ export function ChatList({
         )}
         {!isError &&
           !isLoading &&
-          chats.map((chat) => (
+          chats?.map((chat) => (
             <ChatItem
               key={chat.phone}
               contact={chat}
