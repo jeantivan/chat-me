9f87ed8ed8ef9e4eb091b21640fbf359acd962c4
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-27 20:41:38 -0400

    Crea un nuevo mensaje con texto

---
+++ src/components/CreateMessage.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { IoSend } from "react-icons/io5";
+import { MessageType } from "../types";
+import { CustomIcon } from "./CustomIcon";
+
+const generateRandomId = () => (Math.random() + 1).toString(36).substring(2);
+const getMessageHour = () => {
+  const date = new Date();
+  let minutes =
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+
+  let hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+
+  let terminal = date.getHours() > 12 ? "PM" : "AM";
+
+  return `${hours}:${minutes} ${terminal}`;
+};
+
+const createMessage = (content: string): MessageType => {
+  return {
+    id: generateRandomId(),
+    message: { type: "text", content },
+    status: "send",
+    isOwnMsg: true,
+    reactions: [],
+    time: getMessageHour(),
+  };
+};
+
+interface CreateMessageProps {
+  addMessage: (message: MessageType) => void;
+}
+
+export function CreateMessage({ addMessage }: CreateMessageProps) {
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!content) return;
+    addMessage(createMessage(content));
+    setContent("");
+  };
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setContent(e.currentTarget.value);
+  };
+  return (
+    <form onSubmit={handleSubmit} className="w-full flex">
+      <div className="w-full mx-4 dark:text-gray-400 text-gray-500 dark:bg-slate-600 bg-white rounded-md">
+        <input
+          className="w-full py-2 max-w-full px-3 bg-transparent focus:outline-none dark:text-white text-black"
+          placeholder="Escribe un mensaje aquí"
+          value={content}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="flex items-center">
+        <button
+          type="submit"
+          disabled={!Boolean(content)}
+          className="w-7 h-7 dark:disabled:text-slate-500 disabled:text-slate-600 text-emerald-500 p-0.5"
+        >
+          <CustomIcon Icon={IoSend} label="Enviar mensaje" />
+        </button>
+      </div>
+    </form>
+  );
+}
