0e5ff02b21c61be380381e3bb9cd76bf649830dc
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-27 22:13:42 -0400

    Reesctructurando el componente

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,44 +1,15 @@
-import { useRef, useEffect } from "react";
-import { Message } from "./Message";
-import { MessageType } from "../types";
+import { forwardRef, ReactNode } from "react";
 
-interface MessagesProps {
-  messages: Array<MessageType>;
+interface Props {
+  children?: ReactNode;
 }
+export type Ref = HTMLDivElement;
 
-export function Messages({ messages }: MessagesProps) {
-  const messagesContainer = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (messagesContainer.current) {
-      messagesContainer.current.scrollTo(
-        0,
-        messagesContainer.current.scrollHeight
-      );
-    }
-  }, [messages]);
-
-  return (
-    <div
-      className="messages overflow-y-auto bg-neutral-200 dark:bg-slate-900"
-      ref={messagesContainer}
-    >
-      <div className="flex flex-col-reverse py-5">
-        {messages.map((message, i, array) => {
-          if (i === array.length - 1) {
-            return (
-              <Message key={message.id} {...message} isFirstMessage={true} />
-            );
-          }
-          return (
-            <Message
-              key={message.id}
-              {...message}
-              isFirstMessage={message.isOwnMsg !== array[i + 1].isOwnMsg}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
-}
+export const Messages = forwardRef<Ref, Props>(({ children }, ref) => (
+  <div
+    className="messages overflow-y-auto bg-neutral-200 dark:bg-slate-900"
+    ref={ref}
+  >
+    <div className="flex flex-col-reverse py-5">{children}</div>
+  </div>
+));
