e0bfa6e3cea01add615f103e6be7e0f7bda7b799
Jean Paul Tivan <jptivan53@gmail.com>
2022-07-31 15:16:13 -0400

    ...

--- src/components/Configuration/Privacy.tsx
+++ src/components/Configuration/Privacy.tsx
@@ -1,8 +1,6 @@
 import * as Separator from "@radix-ui/react-separator";
 import * as Label from "@radix-ui/react-label";
-import cx from "classnames";
 import { FiChevronRight } from "react-icons/fi";
-import { useDarkMode } from "../DarkMode";
 import { CustomIcon } from "../CustomIcon";
 import { CheckBox } from "../CheckBox";
 import { Header } from "./Header";
@@ -14,19 +12,16 @@ interface PrivacyProps {
 }
 
 export function Privacy({ goBack }: PrivacyProps) {
-  const { mode } = useDarkMode();
-  const isLight = mode === "light";
-
   return (
     <AnimateOptionChange>
       <Header goBack={goBack}>Privacidad</Header>
       <div className="overflow-y-auto flex-1 bg-neutral-100 dark:bg-slate-900">
         <div className="flex flex-col">
-          <div className="p-6 mb-5 bg-white dark:bg-slate-800">
-            <h2 className="text-emerald-600 mb-3">
+          <div className="py-4 px-6 mb-5 bg-white dark:bg-slate-800">
+            <h2 className="text-emerald-600 mb-4">
               Quién puede ver mi información personal
             </h2>
-            <div className="flex py-3 items-center text-sm">
+            <div className="flex pb-4 items-center text-sm">
               <div className="flex-1">
                 <h3 className="text-neutral-900 dark:text-neutral-50">
                   Última vez en linea.
@@ -40,7 +35,7 @@ export function Privacy({ goBack }: PrivacyProps) {
               />
             </div>
             <Separator.Root className="bg-gray-200 dark:bg-gray-600 w-full h-px" />
-            <div className="flex py-3 items-center text-sm">
+            <div className="flex py-4 items-center text-sm">
               <div className="flex-1">
                 <h3 className="text-neutral-900 dark:text-neutral-50">
                   Foto de perfil.
@@ -56,7 +51,7 @@ export function Privacy({ goBack }: PrivacyProps) {
               />
             </div>
             <Separator.Root className="bg-gray-200 dark:bg-gray-600 w-full h-px" />
-            <div className="flex py-3 items-center text-sm">
+            <div className="flex py-4 items-center text-sm">
               <div className="flex-1">
                 <h3 className="text-neutral-900 dark:text-neutral-50">Info.</h3>
                 <p className="text-gray-500 dark:text-gray-400">
@@ -70,17 +65,11 @@ export function Privacy({ goBack }: PrivacyProps) {
               />
             </div>
             <Separator.Root className="bg-gray-200 dark:bg-gray-600  w-full h-px" />
-            <div className="flex pt-3 items-center">
+            <div className="flex pt-4 items-center">
               <Label.Root asChild htmlFor="preview">
-                <div
-                  className={`select-none mr-2  ${
-                    isLight ? "text-gray-900" : "text-gray-100"
-                  }`}
-                >
+                <div className="select-none mr-2 text-neutral-900 dark:text-neutral-50">
                   Confirmaciones de lectura.
-                  <span
-                    className={`mt-1 select-none text-sm inline-block text-gray-400`}
-                  >
+                  <span className="mt-1 select-none text-sm inline-block text-gray-500 dark:text-gray-400">
                     Si desactivas las confirmaciones de lectura, no podrás
                     enviarlas ni recibirlas. Las confirmaciones de lectura se
                     enviarán siempre en los chats grupales.
@@ -90,9 +79,9 @@ export function Privacy({ goBack }: PrivacyProps) {
               <CheckBox defaultChecked id="preview" />
             </div>
           </div>
-          <div className="p-6 mb-5 bg-white dark:bg-slate-800">
-            <h2 className="text-emerald-600 mb-5">Mensajes temporales</h2>
-            <div className="flex py-3 items-center text-sm">
+          <div className="py-4 px-6 mb-5 bg-white dark:bg-slate-800">
+            <h2 className="text-emerald-600 mb-4">Mensajes temporales</h2>
+            <div className="flex items-center text-sm">
               <div className="flex-1">
                 <h3 className="text-neutral-900 dark:text-neutral-50">
                   Duración predeterminada de los mensajes
@@ -106,7 +95,7 @@ export function Privacy({ goBack }: PrivacyProps) {
               />
             </div>
           </div>
-          <div className="p-6 mb-5 bg-white dark:bg-slate-800">
+          <div className="py-4 px-6 bg-white dark:bg-slate-800">
             <div className="flex py-3 items-center text-sm">
               <div className="flex-1">
                 <h3 className="text-neutral-900 dark:text-neutral-50">
