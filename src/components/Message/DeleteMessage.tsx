79df7f9f2ffe02badaa2a6ac71f82b6dc36ee45f
Jean Paul Tivan <jptivan53@gmail.com>
2022-08-02 21:50:02 -0400

    Se usa para eliminar un mensaje

---
+++ src/components/Message/DeleteMessage.tsx
@@ -0,0 +1,93 @@
+import cx from "classnames";
+import { ReactNode, useState } from "react";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
+import {
+  AlertDialogRoot,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogTitle,
+  AlertDialogDescription,
+} from "../AlertDialog";
+import { MenuItem } from "../Menu";
+import { useDarkMode } from "../DarkMode";
+
+const Button: React.FC<{
+  children: ReactNode;
+  type?: "filled" | "outlined";
+  [x: string]: any;
+}> = ({ children, type = "filled", ...rest }) => (
+  <button
+    {...rest}
+    className={cx(
+      "mr-2 py-2 px-6 min-w-[64px] rounded inline-flex justify-center uppercase font-medium border-solid border",
+      {
+        "border-emerald-400/40 hover:bg-white/5 text-emerald-400":
+          type === "outlined",
+      },
+      {
+        "bg-emerald-500 hover:bg-emerald-400 border-emerald-500 hover:border-emerald-400 text-white":
+          type === "filled",
+      }
+    )}
+  >
+    {children}
+  </button>
+);
+
+interface DeleteMessageProps {
+  msgId: string;
+  deleteMsg: (id: string) => void;
+}
+
+export function DeleteMessage({ msgId, deleteMsg }: DeleteMessageProps) {
+  const { isDarkMode } = useDarkMode();
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <AlertDialogRoot open={openModal} onOpenChange={setOpenModal}>
+      <MenuItem>
+        <AlertDialogTrigger className="px-4 py-2">
+          Eliminar Mensaje
+        </AlertDialogTrigger>
+      </MenuItem>
+      <AlertDialogContent
+        open={openModal}
+        className="w-[60%] md:w-[50%] lg:w-[40%]"
+      >
+        <div className="p-6">
+          <VisuallyHidden.Root>
+            <AlertDialogTitle>Eliminar mensaje</AlertDialogTitle>
+          </VisuallyHidden.Root>
+          <div className="mb-6">
+            <AlertDialogDescription
+              className={cx("text-xl font-light", {
+                "text-neutral-900": !isDarkMode,
+                "text-neutral-50": isDarkMode,
+              })}
+            >
+              ¿Deseas eliminar este mensaje?
+            </AlertDialogDescription>
+          </div>
+          <div className="w-full flex justify-end mt-8">
+            <AlertDialogCancel asChild>
+              <Button type="outlined">Cancelar</Button>
+            </AlertDialogCancel>
+            <AlertDialogAction asChild>
+              <Button
+                type="filled"
+                onClick={() => {
+                  deleteMsg(msgId);
+                }}
+                className="py-2 px-4 rounded-md inline-flex justify-center uppercase"
+              >
+                Eliminar mensaje
+              </Button>
+            </AlertDialogAction>
+          </div>
+        </div>
+      </AlertDialogContent>
+    </AlertDialogRoot>
+  );
+}
