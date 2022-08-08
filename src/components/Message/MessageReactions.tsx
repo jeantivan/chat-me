70d6feac3dcaae6b2175682e0b5144afc0c68aa1
Jean Paul Tivan <jptivan53@gmail.com>
2022-08-07 20:33:37 -0400

    ADD: Sección de las reacciones del mensaje

---
+++ src/components/Message/MessageReactions.tsx
@@ -0,0 +1,35 @@
+import cx from "classnames";
+import { ReactionType } from "../../types";
+import { Reaction } from "../Reactions";
+
+interface MessageReactionsProps {
+  isOwnMsg: boolean;
+  reactions: ReactionType[] | [];
+}
+
+export function MessageReactions({
+  reactions,
+  isOwnMsg,
+}: MessageReactionsProps) {
+  return (
+    <div className="w-full flex items-center min-h-[30px]">
+      <div
+        className={cx(
+          "dark:bg-slate-700 bg-white border border-slate-200 dark:border-slate-800 px-1 py-0.5 rounded-full inline-flex",
+          "min-w-[40px] -mt-2 z-10",
+          {
+            "mr-auto ml-3": !isOwnMsg,
+            "ml-auto mr-3": isOwnMsg,
+          }
+        )}
+      >
+        {reactions.map(({ reaction }) => (
+          <Reaction key={reaction.type} type={reaction.type} />
+        ))}
+        <span className="w-4 text-base text-gray-500 dark:text-gray-400">
+          {reactions.length}
+        </span>
+      </div>
+    </div>
+  );
+}
