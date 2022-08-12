a03c56a1279054b54943452bffe32961b3745ed1
Jean Paul Tivan <jptivan53@gmail.com>
2022-08-11 20:46:35 -0400

    DELETE: Eliminado componente en favor de usar distintos contenidos según el tipo de mensaje

--- src/components/Message/MessageContent.tsx
+++ src/components/Message/MessageContent.tsx
@@ -12,38 +12,40 @@ export function MessageContent({
   time,
 }: MessageType) {
   return (
-    <div className="relative">
-      <div className="relative whitespace-pre-wrap break-words">
-        <span className="text-base dark:text-white leading-tight">
-          {message.content}
-        </span>
-        <span
-          className={cx("inline-block", {
-            "w-[84px]": isFavMsg < 0,
-            "w-[96px]": isFavMsg > 0,
-          })}
-        />
-      </div>
-
-      <div className="relative float-right z-10 -mt-3 -mb-[5px]">
-        <span className="text-xs flex items-end">
-          {isFavMsg === 1 && (
-            <CustomIcon
-              Icon={BsStarFill}
-              className="text-gray-400 self-center inline-block"
-              label="Fav Msg"
-            />
-          )}
-          <span className="dark:text-gray-400 text-neutral-500 mx-1.5 whitespace-nowrap">
-            {time}
+    <div className="pt-1.5 px-3 pb-2.5 h-full">
+      <div className="w-full h-full relative">
+        <div className="relative whitespace-pre-wrap break-words">
+          <span className="text-base dark:text-white leading-tight">
+            {message.content}
           </span>
-          {isOwnMsg && (
-            <MessageStatus
-              status={status}
-              className="w-5 -mb-0.5 inline-block"
-            />
-          )}
-        </span>
+          <span
+            className={cx("inline-block", {
+              "w-[84px]": isFavMsg < 0,
+              "w-[96px]": isFavMsg > 0,
+            })}
+          />
+        </div>
+
+        <div className={cx("relative float-right z-10 -mt-3 -mb-[3px]")}>
+          <span className="text-xs flex items-end">
+            {isFavMsg === 1 && (
+              <CustomIcon
+                Icon={BsStarFill}
+                className="text-gray-400 self-center inline-block"
+                label="Fav Msg"
+              />
+            )}
+            <span className="text-gray-400 mx-1.5 whitespace-nowrap">
+              {time}
+            </span>
+            {isOwnMsg && (
+              <MessageStatus
+                status={status}
+                className="w-5 mr-1 -mb-0.5 inline-block"
+              />
+            )}
+          </span>
+        </div>
       </div>
     </div>
   );
