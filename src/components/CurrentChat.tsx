f711b677802f91d17522b3bf66e82e803d323634
Jean Paul Tivan <jptivan53@gmail.com>
2022-08-13 18:53:36 -0400

    ADD: Context con el Chat abierto actualmente

---
+++ src/components/CurrentChat.tsx
@@ -0,0 +1,30 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useState,
+} from "react";
+import { ChatType } from "../types";
+
+type CurrentChatContextType = {
+  currentChat: ChatType | null;
+  setCurrentChat: Dispatch<SetStateAction<ChatType | null>>;
+};
+
+const CurrentChatContext = createContext<CurrentChatContextType>(
+  {} as CurrentChatContextType
+);
+
+export const useCurrentChat = () => useContext(CurrentChatContext);
+
+export function CurrentChatProvider({ children }: { children: ReactNode }) {
+  const [currentChat, setCurrentChat] = useState<ChatType | null>(null);
+
+  return (
+    <CurrentChatContext.Provider value={{ currentChat, setCurrentChat }}>
+      {children}
+    </CurrentChatContext.Provider>
+  );
+}
