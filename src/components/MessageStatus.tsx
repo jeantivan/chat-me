9adc7119b744dbcdc1e1fda08ed39bf0e896eb09
Jean Paul Tivan <jptivan53@gmail.com>
2022-08-23 20:47:34 -0400

    ADD: Agregado estado "idle" inicial a MessageStatus

--- src/components/MessageStatus.tsx
+++ src/components/MessageStatus.tsx
@@ -1,15 +1,20 @@
 import cx from "classnames";
-import { BsCheck, BsCheckAll } from "react-icons/bs";
+import { BsCheck, BsCheckAll, BsClock } from "react-icons/bs";
 import { CustomIcon } from "./CustomIcon";
 
 interface MessageStatusProps {
-  status: "read" | "received" | "send";
+  status: "idle" | "read" | "received" | "send";
   className?: string;
 }
 
 export function MessageStatus({ status, className }: MessageStatusProps) {
   let icon = BsCheckAll;
   let label = "recibido";
+
+  if (status === "idle") {
+    icon = BsClock;
+    label = "enviando";
+  }
 
   if (status === "send") {
     icon = BsCheck;
