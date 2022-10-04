9f57c408ee6d11f750fe46be3606c6f7c9e9aaa7
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-04 00:08:37 -0400

    NEW: Exporta los "sliceCreator" de zustand

---
+++ src/store/slices/index.tsx
@@ -0,0 +1,4 @@
+export { createChatsSlice } from "./ChatsSlice";
+export { createContactsSlice } from "./ContactsSlice";
+export { createProfileSlice } from "./ProfileSlice";
+export { createConfigSlice } from "./ConfigSlice";
