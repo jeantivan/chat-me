dd329d47cf8ca08869f84398c5f8733db7a982e8
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-04 23:44:13 -0400

    ADD: Agregadas las interfaces de slice de ChatId; Chat y Reacciones

--- src/store/slices/interfaces/index.ts
+++ src/store/slices/interfaces/index.ts
@@ -1,12 +1,16 @@
-import { ConfigType, UserType, ChatType } from "./../../../types";
+import { ConfigType, UserType, ChatType, MessageType } from "./../../../types";
 
+// Interface para todos los Contactos
 export interface ContactsSlice {
   contacts: UserType[];
 }
+
+// Interface para todos los Chats
 export interface ChatsSlice {
   chats: ChatType[];
 }
 
+// Interface de perfil
 export interface ProfileSlice {
   profile: UserType;
   changeName: (name: string) => void;
@@ -14,6 +18,7 @@ export interface ProfileSlice {
   changePicture: (picture: string) => void;
 }
 
+// Interface para la configuración
 export interface ConfigSlice {
   config: ConfigType;
   toggleNotifications: (
@@ -22,8 +27,30 @@ export interface ConfigSlice {
   toggleSecurityNoti: () => void;
 }
 
+// Interface del Id del Chat actual
+export interface CurrentChatIdSlice {
+  currentChatId?: string;
+  setCurrentChatId: (chatId: string) => void;
+  closeChat: () => void;
+}
+
+// Interface para el Chat
+export interface ChatSlice {
+  pinChat: (chatId: string) => void;
+  muteChat: (chatId: string) => void;
+  getAllMessages: () => void;
+  addMessage: (message: MessageType) => void;
+  deleteMessage: (messageId: string) => void;
+  toggleFavMessage: (messageId: string) => void;
+}
+
+// TODO: Interface para las reacciones a los mensajes
+export interface ReactionsSlice {}
+
 export interface StoreSlice
   extends ContactsSlice,
     ChatsSlice,
     ConfigSlice,
-    ProfileSlice {}
+    ProfileSlice,
+    CurrentChatIdSlice,
+    ChatSlice {}
