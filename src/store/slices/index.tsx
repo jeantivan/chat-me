2430e2ddb78da0924882ad2356576e558a0874f5
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-04 23:44:54 -0400

    ADD: export del sliceCreator para CurrentChatId

--- src/store/slices/index.tsx
+++ src/store/slices/index.tsx
@@ -2,3 +2,4 @@ export { createChatsSlice } from "./ChatsSlice";
 export { createContactsSlice } from "./ContactsSlice";
 export { createProfileSlice } from "./ProfileSlice";
 export { createConfigSlice } from "./ConfigSlice";
+export { createCurrentChatIdSlice } from "./CurrentChatIdSlice";
