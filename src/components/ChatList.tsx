0fa47d1c1a53e5168e49fdafa790c4dd740717ce
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-05 00:00:23 -0400

    ADD: Lista de chats viene de la store de zustand

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,3 +1,4 @@
+import useStore from "../store";
 import { ChatType } from "../types";
 import { ChatItem } from "./ChatItem";
 
@@ -7,25 +8,15 @@ interface ChatListProps {
   isLoading: boolean;
 }
 
-export function ChatList({ chats, isError, isLoading }: ChatListProps) {
+export function ChatList() {
+  const chats = useStore((state) => state.chats);
+
   return (
     <div className="chats-list h-full overflow-x-hidden bg-white dark:bg-slate-800/80 border-solid border-r border-slate-200 dark:border-slate-600 flex flex-col">
       <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-        {isError && (
-          <div className="py-4 mx-auto text-lg dark:text-white">
-            Ups algo salió mal
-          </div>
-        )}
-        {isLoading && (
-          <div className="py-4 mx-auto text-lg dark:text-white">
-            Cargando...
-          </div>
-        )}
-        {!isError &&
-          !isLoading &&
-          chats?.map((chat) => (
-            <ChatItem key={chat.contact.phone} chat={chat} />
-          ))}
+        {chats.map((chat) => (
+          <ChatItem key={chat.id} id={chat.id} />
+        ))}
       </div>
     </div>
   );
