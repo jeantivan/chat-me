274ec353a4ffd4b516eae14311ba362335fb7f4f
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-06 21:58:20 -0400

    NEW: Contenedor de los componentes del chat

---
+++ src/containers/Chat.tsx
@@ -0,0 +1,50 @@
+import { ReactNode } from "react";
+import {
+  Welcome,
+  Messages,
+  InputContainer,
+  ChatHeader,
+  CreateMessage,
+  ContactInfoContent,
+  ContactInfoProvider,
+} from "../components";
+import useStore from "../store";
+
+const Container = ({ children }: { children: ReactNode }) => (
+  <ContactInfoProvider>
+    <section className="flex-1 w-full flex">{children}</section>
+  </ContactInfoProvider>
+);
+
+export function Chat() {
+  const chat = useStore((state) =>
+    state.chats.find((chat) => chat.id === state.currentChatId)
+  );
+
+  const getAllMessages = useStore((state) => state.getAllMessages);
+
+  if (chat?.shouldLoadOldMsg) {
+    getAllMessages();
+  }
+
+  return (
+    <Container>
+      {!chat && <Welcome />}
+      {chat && (
+        <>
+          <div className="chat-container flex-1">
+            <ChatHeader chat={chat} />
+            <Messages
+              messages={chat.messages}
+              shouldLoadOldMsg={chat.shouldLoadOldMsg}
+            />
+            <InputContainer>
+              <CreateMessage chatId={chat.id} />
+            </InputContainer>
+          </div>
+          <ContactInfoContent chat={chat} />
+        </>
+      )}
+    </Container>
+  );
+}
