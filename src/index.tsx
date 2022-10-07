17ac796802a092b3cd5f50073f7f8efed157055b
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-06 20:13:09 -0400

    DELETE: Descartados los providers de context innecesarios

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {
-  DarkModeProvider,
-  UserInfoProvider,
-  ContactInfoProvider,
-  LeftDrawerProvider,
-  CurrentChatProvider,
-} from "./components";
+import { DarkModeProvider } from "./components";
 
 import { QueryClient, QueryClientProvider } from "react-query";
 
@@ -21,15 +15,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <DarkModeProvider>
-        <LeftDrawerProvider>
-          <UserInfoProvider>
-            <ContactInfoProvider>
-              <CurrentChatProvider>
-                <App />
-              </CurrentChatProvider>
-            </ContactInfoProvider>
-          </UserInfoProvider>
-        </LeftDrawerProvider>
+        <App />
       </DarkModeProvider>
     </QueryClientProvider>
   </React.StrictMode>
