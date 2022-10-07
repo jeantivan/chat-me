87ea4a1dec6449bc1cc11f66d9bc5baf29a6571c
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-06 20:36:25 -0400

    CHANGE: Refactorización los la lógica estará en los contenedores

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,12 @@
-import { useEffect } from "react";
 import "./App.css";
-import {
-  useDarkMode,
-  ChatList,
-  ChatHeader,
-  InputContainer,
-  Messages,
-  UserProfile,
-  Welcome,
-  SearchChats,
-  LeftDrawerContent,
-  ContactInfoContent,
-  CreateMessage,
-} from "./components";
-
-import useStore from "./store";
-import { MessageType } from "./types";
+import { Main, HeaderAndChatList, Chat } from "./containers";
 
 function App() {
-  const { isDarkMode } = useDarkMode();
-
-  const currentChatId = useStore((state) => state.currentChatId);
-
   return (
-    <div
-      className={`${
-        isDarkMode ? "dark" : "light"
-      } md:overflow-x-auto max-h-screen min-w-screen flex`}
-    >
-      <main className="app-container min-w-md w-full dark:bg-slate-900 overflow-hidden flex">
-        <section className="user-chats relative">
-          <UserProfile />
-          <SearchChats />
-          <ChatList />
-          <LeftDrawerContent />
-        </section>
-        <section className="flex-1 w-full flex">
-          <div className="chat-container flex-1">
-            {!currentChatId ? (
-              <Welcome />
-            ) : (
-              <>
-                <ChatHeader />
-                <Messages />
-
-                <InputContainer>
-                  <CreateMessage />
-                </InputContainer>
-              </>
-            )}
-          </div>
-          <ContactInfoContent />
-        </section>
-      </main>
-    </div>
+    <Main>
+      <HeaderAndChatList />
+      <Chat />
+    </Main>
   );
 }
 
