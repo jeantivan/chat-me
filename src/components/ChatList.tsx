76aa8c522e50ccd2f605c653e1f9fa636bf43cdd
Jean Paul Tivan <jptivan53@gmail.com>
2022-10-08 00:24:56 -0400

    ADD: Ordena los chats: Primero los chat pineados

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,3 +1,4 @@
+import { orderBy, sortBy } from "lodash";
 import useStore from "../store";
 import { ChatType } from "../types";
 import { ChatItem } from "./ChatItem";
@@ -11,10 +12,12 @@ interface ChatListProps {
 export function ChatList() {
   const chats = useStore((state) => state.chats);
 
+  const orderedChat = orderBy(chats, ["isPinned"], ["desc"]);
+
   return (
     <div className="chats-list h-full overflow-x-hidden bg-white dark:bg-slate-800/80 border-solid border-r border-slate-200 dark:border-slate-600 flex flex-col">
       <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-        {chats.map((chat) => (
+        {orderedChat.map((chat) => (
           <ChatItem key={chat.id} chat={chat} />
         ))}
       </div>
