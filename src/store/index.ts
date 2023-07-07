9d9506290ca700db560228fa03ff7278ef8d1696
jeantivan <jptivan53@gmail.com>
2023-07-07 18:15:40 -0400

    CHANGE: "create" ya no es un default import

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import create from "zustand";
+import { create } from "zustand";
 import { persist, devtools } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 import { StoreSlice } from "./slices/interfaces";
