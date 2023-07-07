39834135ef45567d490426d6b11f99e11768e51c
jeantivan <jptivan53@gmail.com>
2023-07-07 17:06:53 -0400

    UPDATE: Se usa react con swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
+import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     open: true,
   },
-  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
