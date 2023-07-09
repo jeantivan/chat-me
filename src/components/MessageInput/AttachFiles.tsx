94041c8717aba25c5ce56a94713915f1dfe9a2b2
jeantivan <jptivan53@gmail.com>
2023-07-08 21:36:39 -0400

    ADD: Menu para adjuntar archivos

---
+++ src/components/MessageInput/AttachFiles.tsx
@@ -0,0 +1,9 @@
+import { AttachFilesMenu } from "@/components/AttachFilesMenu";
+
+export function AttachFiles() {
+  return (
+    <div className="flex items-center">
+      <AttachFilesMenu />
+    </div>
+  );
+}
