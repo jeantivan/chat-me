1e8bc396d807ca1479654d79a0c3c67ce6408ad4
jeantivan <jptivan53@gmail.com>
2023-07-11 00:59:49 -0400

    CHANGE: Se usa IconButton. CHANGE: Muestra las opciones de configuración en el leftDrawer

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,26 +1,22 @@
 import cx from "classnames";
 import { useState } from "react";
-import { BsThreeDotsVertical } from "react-icons/bs";
+import { MoreVertical } from "lucide-react";
 import { MenuRoot, MenuTrigger, MenuContent, MenuItem } from "./Menu";
-import { CustomIcon } from "./CustomIcon";
-import { useLeftDrawer } from "./LeftDrawer";
+import { IconButton } from "./ui/IconButton";
+import useStore from "@/lib/store";
 
 export function UserMenu() {
   const [open, setOpen] = useState(false);
 
-  const { openLeftDrawerAndRenderContent } = useLeftDrawer();
+  const leftDrawerGoTo = useStore((state) => state.leftDrawerGoTo);
 
   return (
     <MenuRoot open={open} onOpenChange={setOpen}>
       <MenuTrigger
-        className={cx(
-          "h-8 w-8 rounded-full p-2",
-          "dark:text-gray-400 text-gray-500",
-          "hover:bg-slate-200 dark:hover:bg-gray-600",
-          { "dark:bg-gray-600 bg-gray-200": open }
-        )}
+        asChild
+        className={cx(open && "dark:bg-gray-600 bg-gray-200")}
       >
-        <CustomIcon icon={BsThreeDotsVertical} label="Abrir menu" />
+        <IconButton icon={MoreVertical} label="Abrir menu" />
       </MenuTrigger>
       <MenuContent align="end" className="w-56">
         <MenuItem>Nuevo grupo</MenuItem>
@@ -28,7 +24,7 @@ export function UserMenu() {
         <MenuItem>Mensajes destacados</MenuItem>
         <MenuItem
           onClick={() => {
-            openLeftDrawerAndRenderContent("CONFIGURATION");
+            leftDrawerGoTo("OPTIONS");
           }}
         >
           Configuración
