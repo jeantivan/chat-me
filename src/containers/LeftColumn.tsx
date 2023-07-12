14907d842574981b13fb03828a18ac0df32e58fe
jeantivan <jptivan53@gmail.com>
2023-07-12 00:28:15 -0400

    CHANGE: SearchChats movido dentro de ChatList

--- src/containers/LeftColumn.tsx
+++ src/containers/LeftColumn.tsx
@@ -1,4 +1,4 @@
-import { ChatList, SearchChats, UserProfile } from "@/components";
+import { ChatList, UserProfile } from "@/components";
 import { LeftDrawer, LeftDrawerElement } from "@/components/LeftDrawer";
 
 import { Profile } from "@/components/Configuration/Profile";
@@ -15,7 +15,7 @@ export function LeftColumn() {
   return (
     <section id="left-column" className="user-chats relative">
       <UserProfile />
-      <SearchChats />
+
       <ChatList />
       <LeftDrawer>
         <LeftDrawerElement option="PROFILE" Component={Profile} />
