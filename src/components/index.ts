8a4ee50a220e4195d6f3cf1ecfdbd5adc8065f16
jeantivan <jptivan53@gmail.com>
2023-07-12 18:20:59 -0400

    DELETE: Componente sin usar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { DarkModeProvider, useDarkMode, DarkModeContext } from "./DarkMode";
 export { ChatList } from "./ChatList";
 export { ChatHeader } from "./ChatHeader";
-export { InputContainer } from "./InputContainer";
 export { Messages } from "./Messages";
 export { UserProfile } from "./UserProfile";
 export { Welcome } from "./Welcome";
