89fd6775709dc3115a64dc6b7aaa0d85dc28c002
jeantivan <jptivan53@gmail.com>
2023-07-11 20:42:02 -0400

    FIX: Extiende los maxWidth y minWidth en lugar de reescribirlos

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
-    maxWidth: {
-      "7/10": "70%",
-      "8/10": "80%",
-      "9/10": "90%",
+    extend: {
+      maxWidth: {
+        "7/10": "70%",
+        "8/10": "80%",
+        "9/10": "90%",
+      },
+      minWidth: {
+        md: "768px",
+      },
     },
-    minWidth: {
-      md: "768px",
-    },
-
-    extend: {},
   },
   plugins: [require("tailwindcss-radix")()],
 };
