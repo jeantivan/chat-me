edd04a0ca126d1a1bd94129266f0de371fcbb8d6
jeantivan <jptivan53@gmail.com>
2023-07-13 02:15:45 -0400

    CHANGE: Agregado el nuevo menu. Nuevas acciones con el RightDrawer

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -1,26 +1,22 @@
-import { useEffect } from "react";
-import { BsSearch } from "react-icons/bs";
-import { ContactMenu } from "./ContactMenu";
-import { useContactInfo } from "./ContactInfo";
+import { Search } from "lucide-react";
+
+import { IconButton } from "./ui/IconButton";
+import { ChatHeaderMenu } from "./ChatHeaderMenu";
 import { ChatType } from "@/lib/types";
+import useStore from "@/lib/store";
 
 export function ChatHeader({ chat }: { chat: ChatType }) {
-  const { setOpenContactInfo } = useContactInfo();
-
+  const rightDrawerGoTo = useStore((state) => state.rightDrawerGoTo);
   const { name, picture } = chat.contact;
-
-  useEffect(() => {
-    setOpenContactInfo(false);
-  }, [chat.isOpenChat]);
 
   return (
     <header className="chat-header bg-white dark:bg-slate-700 shadow-lg">
       <div className="flex items-center px-5 py-2">
         <button
-          onClick={() => {
-            setOpenContactInfo(true);
-          }}
           className="flex-1 flex items-stretch"
+          onClick={() => {
+            rightDrawerGoTo("CONTACT_INFO");
+          }}
         >
           <span className="text-gray-400 w-10 h-10 rounded-full mr-3">
             <img
@@ -35,13 +31,9 @@ export function ChatHeader({ chat }: { chat: ChatType }) {
             </span>
           </span>
         </button>
-        <div className="flex items-center">
-          <div className="h-9 w-9 ml-2 rounded-full dark:text-gray-400 text-gray-500 hover:bg-slate-200 dark:hover:bg-slate-600 p-2">
-            <BsSearch className="h-full w-full" />
-          </div>
-          <div className="ml-2">
-            <ContactMenu chat={chat} />
-          </div>
+        <div className="flex items-center gap-2">
+          <IconButton icon={Search} label="Buscar" />
+          <ChatHeaderMenu />
         </div>
       </div>
     </header>
