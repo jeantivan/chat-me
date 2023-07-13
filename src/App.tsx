a1b08fcef5e339a9d9e3ca748651e4b2e77b7a52
jeantivan <jptivan53@gmail.com>
2023-07-13 02:13:51 -0400

    ADD: Columna Derecha

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Main, LeftColumn, Chat } from "@/containers";
+import { RightColumn } from "./containers/RightColumn";
 
 function App() {
   return (
     <Main>
       <LeftColumn />
-      <Chat />
+
+      <RightColumn />
     </Main>
   );
 }
