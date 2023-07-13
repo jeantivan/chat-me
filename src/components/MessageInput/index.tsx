9b2c6480cecf55b23167a0536a7a7a40d137d982
jeantivan <jptivan53@gmail.com>
2023-07-13 16:19:09 -0400

    CHANGE: Boton para los iconos dentro junto al editor

--- src/components/MessageInput/index.tsx
+++ src/components/MessageInput/index.tsx
@@ -19,23 +19,23 @@ const Placeholder = () => (
 export function MessageInput() {
   return (
     <Root>
-      <button className="w-11 h-11 p-2 inline-flex rounded-full dark:text-slate-400 text-slate-600">
-        <CustomIcon
-          icon={Laugh}
-          label="Insertar emojis"
-          className="w-full h-full"
-        />
-      </button>
       <AttachFiles />
       <div className="flex-1 relative min-h-[44px] flex overflow-y-auto dark:text-gray-400 text-gray-500 dark:bg-slate-600 bg-white rounded-md">
-        <div className="grid items-center w-full max-h-[136px] py-2 max-w-full px-3">
+        <div className="flex items-end w-full max-h-[136px] py-2 max-w-full px-3">
           <RichTextPlugin
             contentEditable={
-              <ContentEditable className="col-span-1 w-full bg-transparent focus:outline-none dark:text-white text-black" />
+              <ContentEditable className="w-full bg-transparent focus:outline-none dark:text-white text-black" />
             }
             placeholder={<Placeholder />}
             ErrorBoundary={LexicalErrorBoundary}
           />
+          <button className="w-7 h-7 inline-flex rounded-full dark:text-slate-400 text-slate-600">
+            <CustomIcon
+              icon={Laugh}
+              label="Insertar emojis"
+              className="w-full h-full"
+            />
+          </button>
         </div>
       </div>
       <SendMessage />
