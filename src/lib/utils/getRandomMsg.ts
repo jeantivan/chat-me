9a4ef99a16cd9d6c28efd5d02f246acedc8ed3d5
jeantivan <jptivan53@gmail.com>
2023-07-22 15:49:08 -0400

    DELETE: Utils sin uso en el proyecto

--- src/lib/utils/getRandomMsg.ts
+++
@@ -1,7 +0,0 @@
-import { MessageType } from "../types";
-import LAST_MESSAGES from "../../assets/mock-data/last-messages.json";
-
-export const getRandomMsg = (): MessageType =>
-  LAST_MESSAGES[
-    Math.floor(Math.random() * LAST_MESSAGES.length)
-  ] as MessageType;
--- src/lib/utils/secondsToMin.ts
+++
@@ -1,8 +0,0 @@
-export const secondsToMin = (time: number) => {
-  const minutes = Math.floor(time / 60);
-  const secondsLeft = time - minutes * 60;
-
-  return `${minutes < 1 ? "00" : minutes}:${
-    secondsLeft < 10 ? "0" + secondsLeft : secondsLeft
-  }`;
-};
