02f2124687f3aa12c91d9388f8ef0e95d0ab582e
jeantivan <jptivan53@gmail.com>
2023-07-21 21:04:02 -0400

    CHANGE: Recibe el mensaje en una sola prop

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -4,12 +4,13 @@ import { MessageText } from "./MessageText";
 import { ImageAndVideo } from "./ImageAndVideo";
 import { TMessage } from "@/lib/types";
 
-type MessageProps = TMessage & {
+type MessageProps = {
   isOwnMsg: boolean;
   hasTail: boolean;
+  message: TMessage;
 };
 export const Message = memo(function MessageRoot(props: MessageProps) {
-  const { body, hasMedia } = props;
+  const { body, hasMedia } = props.message;
 
   return (
     <MessageContainer {...props}>
