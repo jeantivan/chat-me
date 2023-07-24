a78616829c797ad76d36245ef582269113616bb3
jeantivan <jptivan53@gmail.com>
2023-07-24 18:06:07 -0400

    ADD: Ordena y filtra los chats dependiendo de la fecha del ultimo mensaje o si esta fijado

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,18 +1,33 @@
-import { orderBy } from "lodash";
+import orderBy from "lodash/orderBy";
+import partition from "lodash/partition";
 import { ChatItem } from "./ChatItem";
-import useStore from "@/lib/store";
 import { SearchChats } from "./SearchChats";
+import useStore from "@/lib/store";
+import { TChat } from "@/lib/types";
+
+const getDateOfLastMessage = (c: TChat) => {
+  const date = c.messages[c.messages.length - 1].time;
+
+  return new Date(date);
+};
 
 export function ChatList() {
   const chats = useStore((state) => state.chats);
 
-  const orderedChat = orderBy(chats, ["pinned"], ["desc"]);
+  const orderChats = orderBy(chats, [getDateOfLastMessage], ["desc"]);
+
+  const noArchivedChats = orderChats.filter((chat) => !chat.archived);
+
+  const [pinnedChats, restChats] = partition(noArchivedChats, { pinned: true });
 
   return (
     <>
       <SearchChats />
       <div className="chats-list h-full p-2 overflow-x-hidden overflow-y-auto bg-slate-50 dark:bg-slate-800/80 border-solid border-r border-slate-200 dark:border-slate-600">
-        {orderedChat.map((chat) => (
+        {pinnedChats.map((chat) => (
+          <ChatItem key={chat.id} chat={chat} />
+        ))}
+        {restChats.map((chat) => (
           <ChatItem key={chat.id} chat={chat} />
         ))}
       </div>
