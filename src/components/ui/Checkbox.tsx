53a914624c6512b1430cdf5d25b7fa8f608a451d
jeantivan <jptivan53@gmail.com>
2023-07-25 00:37:44 -0400

    ADD: Estilos para el estado deshabilitado del comp

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -8,13 +8,15 @@ export function Checkbox(props: CheckboxPrimitive.CheckboxProps) {
     <CheckboxPrimitive.Root
       {...props}
       className={mc(
-        "flex h-5 w-5 min-w-[20px] items-center justify-center rounded border-2",
+        "flex h-5 w-5 min-w-[20px] items-center justify-center rounded border-2 transition",
         "border-gray-400 bg-transparent",
-        "data-[state=checked]:bg-emerald-500 data-[state=checked]:border-emerald-500",
+        !props.disabled &&
+          "data-[state=checked]:bg-emerald-500 data-[state=checked]:border-emerald-500",
+        "disabled:bg-gray-400/80 dark:disabled:bg-gray-500",
         "focus:outline-none focus-visible:ring focus-visible:ring-emerald-500 focus-visible:ring-opacity-75"
       )}
     >
-      <CheckboxPrimitive.Indicator>
+      <CheckboxPrimitive.Indicator className="transition-all data-[disabled]:hidden">
         <Check className="h-[18px] w-[18px] self-center text-white" />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
