74f775629508595caa9a1c25725e6700dc95acf3
jeantivan <jptivan53@gmail.com>
2023-07-29 15:09:51 -0400

    CHANGE: Color del estatus 'read'

--- src/components/Message/Status.tsx
+++ src/components/Message/Status.tsx
@@ -1,23 +1,21 @@
 import { Clock, Check, CheckCheck, LucideIcon } from "lucide-react";
 import { CustomIcon } from "../CustomIcon";
-import { MessageType } from "@/lib/types";
+import { TMessage } from "@/lib/types";
 import mc from "@/lib/utils/mergeClassnames";
 
-const STATUS: Record<
-  MessageType["status"],
-  { icon: LucideIcon; label: string }
-> = {
-  idle: { icon: Clock, label: "Enviando mensaje" },
-  send: { icon: Check, label: "Mensaje enviado" },
-  received: { icon: CheckCheck, label: "Mensaje recibido" },
-  read: { icon: CheckCheck, label: "Mensaje leído" },
-};
+const STATUS: Record<TMessage["status"], { icon: LucideIcon; label: string }> =
+  {
+    idle: { icon: Clock, label: "Enviando mensaje" },
+    send: { icon: Check, label: "Mensaje enviado" },
+    received: { icon: CheckCheck, label: "Mensaje recibido" },
+    read: { icon: CheckCheck, label: "Mensaje leído" },
+  };
 
 export function Status({
   status,
   className,
 }: {
-  status: MessageType["status"];
+  status: TMessage["status"];
   className?: string;
 }) {
   const { icon, label } = STATUS[status];
@@ -28,7 +26,7 @@ export function Status({
       label={label}
       className={mc(
         "w-4 h-4 text-slate-400",
-        status === "read" && "text-cyan-400",
+        status === "read" && "text-cyan-600 dark:text-cyan-400",
         className
       )}
     />
