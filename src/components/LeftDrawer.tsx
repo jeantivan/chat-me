22e851e581d872bfc66a631418e94465083104a2
jeantivan <jptivan53@gmail.com>
2023-07-30 00:39:37 -0400

    CHANGE: Border innecesario

--- src/components/LeftDrawer.tsx
+++ src/components/LeftDrawer.tsx
@@ -62,7 +62,7 @@ export function LeftDrawer({ children }: { children: ReactNode }) {
             id="left-drawer-dialog"
             aria-labelledby="left-drawer-title"
             tabIndex={-1}
-            className="bg-background-1 border-r border-background-7 w-full h-full flex flex-col pointer-events-auto"
+            className="bg-background-1 w-full h-full flex flex-col pointer-events-auto"
           >
             <AnimatePresence mode="wait">{children}</AnimatePresence>
           </div>
