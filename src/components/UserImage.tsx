7444ca4e3881a36dc0e7d1cb46df82ca351e0432
jeantivan <jptivan53@gmail.com>
2023-08-01 00:40:43 -0400

    CHANGE: Usa el Avatar de @radix-ui

--- src/components/UserImage.tsx
+++ src/components/UserImage.tsx
@@ -1,21 +1,23 @@
-import { FaUserCircle } from "react-icons/fa";
-import { CustomIcon } from "./CustomIcon";
-import useStore from "@/lib/store";
+import * as Avatar from "@radix-ui/react-avatar";
+import { UserCircle2 } from "lucide-react";
 
-export function UserImage() {
+import useStore from "@/lib/store";
+import mc from "@/lib/utils/mergeClassnames";
+
+export function UserImage({ className }: { className?: string }) {
   const picture = useStore((state) => state.user.picture);
 
-  return !picture ? (
-    <CustomIcon
-      label="Avatar"
-      icon={FaUserCircle}
-      className="w-full h-full text-gray-400"
-    />
-  ) : (
-    <img
-      alt="Tu foto de perfil."
-      src={picture}
-      className="w-full h-full bg-gray-400"
-    />
+  return (
+    <Avatar.Root
+      className={mc("bg-background-6 rounded-full overflow-hidden", className)}
+    >
+      <Avatar.Image src={picture} alt="Tu foto de perfil"></Avatar.Image>
+      <Avatar.Fallback className="w-full h-full">
+        <UserCircle2
+          className="w-full h-full text-neutral-400"
+          strokeWidth={1}
+        />
+      </Avatar.Fallback>
+    </Avatar.Root>
   );
 }
