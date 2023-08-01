318ab3ea0ff08a29f6322901bc28e2b390078492
jeantivan <jptivan53@gmail.com>
2023-08-01 02:22:59 -0400

    ADD: Context del custom container de los dialogs

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,15 +1,18 @@
 import { ReactNode } from "react";
 import cx from "classnames";
 import { useTheme } from "@/lib/hooks";
+import { DialogContainer } from "@/components/DialogContainer";
 
 export function Main({ children }: { children: ReactNode }) {
   const { mode, colors } = useTheme();
 
   return (
     <div className={cx(mode, colors)}>
-      <main className="app-container min-w-md w-full bg-background-1 overflow-hidden flex">
-        {children}
-      </main>
+      <DialogContainer>
+        <main className="app-container min-w-md w-full bg-background-1 overflow-hidden flex">
+          {children}
+        </main>
+      </DialogContainer>
     </div>
   );
 }
