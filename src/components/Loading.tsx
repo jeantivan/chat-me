48436c04a1e33d7585d71d980812fe94f8fb40d9
jeantivan <jptivan53@gmail.com>
2023-08-09 22:54:32 -0400

    ADD: Nuevas colores basado en el tema

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -9,17 +9,13 @@ export function Loading() {
     prepareApp();
   }, []);
   return (
-    <div className="w-full h-full grid place-items-center bg-slate-200 dark:bg-slate-700">
+    <div className="w-full h-full grid place-items-center bg-background-2 text-background-12">
       <div>
-        <h1 className="text-4xl text-slate-950 dark:text-slate-50 mb-2">
-          Welcome to ChatMe!
+        <h1 className="text-4xl mb-2">
+          Welcome to <span className="text-primary-10">ChatMe!</span>
         </h1>
-        <p className="text-2xl text-slate-700 dark:text-slate-300">
-          Por favor espera mientras preparamos todo
-        </p>
-        <div className="text-xl text-slate-700 dark:text-slate-300">
-          {loading}
-        </div>
+        <p className="text-2xl">Por favor espera mientras preparamos todo</p>
+        <div className="text-xl text-background-11">{loading}</div>
       </div>
     </div>
   );
