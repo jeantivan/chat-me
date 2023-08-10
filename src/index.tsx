e8995b13d29e54fb0edaa59733dec4643dcfaab8
jeantivan <jptivan53@gmail.com>
2023-08-09 23:34:19 -0400

    DELETE: Providers sin uso

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/styles/index.css";
 import App from "./App";
-import { DarkModeProvider } from "@/components";
-
-import { QueryClient, QueryClientProvider } from "react-query";
-
-const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <DarkModeProvider>
-        <App />
-      </DarkModeProvider>
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
