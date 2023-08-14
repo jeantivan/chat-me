3e20c09a61c5b2ce5e19b8a5fd4e7376fc73aeb9
jeantivan <jptivan53@gmail.com>
2023-08-14 18:31:23 -0400

    CHANGE: Export reemplazados por el comp de ./Chat

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { ChatList } from "./ChatList";
-export { ChatHeader } from "./ChatHeader";
-export { Messages } from "./Messages";
+export { Chat } from "./Chat";
 export { UserProfile } from "./UserProfile";
 export { Welcome } from "./Welcome";
 export { SearchChats } from "./SearchChats";
@@ -8,4 +7,3 @@ export { SearchChats } from "./SearchChats";
 export { LeftDrawer, LeftDrawerElement } from "./LeftDrawer";
 export { RightDrawer, RightDrawerElement } from "./RightDrawer";
 export { Message } from "./Message";
-export { MessageInput } from "./MessageInput";
