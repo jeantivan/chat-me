73188b85d7827bf24f057a6c77fc748865cdf19f
jeantivan <jptivan53@gmail.com>
2023-08-14 16:50:25 -0400

    CHANGE: Se usa grid para las opciones. Header ya no es necesario

--- src/components/Configuration/Theme.tsx
+++ src/components/Configuration/Theme.tsx
@@ -1,9 +1,7 @@
 import * as RadioGroup from "@radix-ui/react-radio-group";
 import { Moon, Sun } from "lucide-react";
-import { Header } from "./Header";
 
 import mc from "@/lib/utils/mergeClassnames";
-import useStore from "@/lib/store";
 import { useTheme } from "@/lib/hooks";
 import { TTheme } from "@/lib/types";
 
@@ -25,7 +23,7 @@ const ColorItem = (props: ColorItemProps) => (
   <RadioGroup.Item
     {...props}
     className={mc(
-      "shrink-0 grow w-[calc(30%-1rem)] p-1 inline-flex flex-col items-center gap-0.5",
+      "w-full p-1 inline-flex flex-col items-center gap-0.5",
       "text-sm leading-none text-background-11",
       "border rounded",
       "border-background-6 data-[state=checked]:border-primary-7"
@@ -52,46 +50,42 @@ const ModeItem = (props: RadioGroup.RadioGroupItemProps) => {
 };
 
 export function Theme() {
-  const closeLeftDrawer = useStore((state) => state.closeLeftDrawer);
   const { colors, mode, changeThemeColors, changeThemeMode } = useTheme();
 
   return (
-    <>
-      <Header goBack={closeLeftDrawer}>Personalizar interfaz</Header>
-      <div className="overflow-y-auto flex-1 bg-background-1 flex flex-col p-4">
-        <div className="mb-5">
-          <p className="text-primary-10 mb-4">Color principal</p>
-          <RadioGroup.Root
-            className="w-full flex flex-wrap justify-between gap-4"
-            value={colors}
-            onValueChange={(newValue) => {
-              changeThemeColors(newValue as typeof colors);
-            }}
-          >
-            {(Object.keys(options) as Array<TTheme["colors"]>).map((option) => (
-              <ColorItem key={option} value={option} option={options[option]} />
-            ))}
-          </RadioGroup.Root>
-        </div>
-        <div className="mb-5">
-          <p className="text-primary-10 mb-4">Modo</p>
-          <RadioGroup.Root
-            value={mode}
-            onValueChange={(newValue) => {
-              changeThemeMode(newValue as typeof mode);
-            }}
-            className="flex"
-          >
-            <ModeItem value="light">
-              <Sun className="w-4 h-4" /> Claro
-            </ModeItem>
-            <ModeItem value="dark">
-              <Moon className="w-4 h-4" />
-              Oscuro
-            </ModeItem>
-          </RadioGroup.Root>
-        </div>
+    <div className="overflow-y-auto flex-1 bg-background-1 flex flex-col p-4">
+      <div className="mb-5">
+        <p className="text-primary-10 mb-4">Color principal</p>
+        <RadioGroup.Root
+          className="w-full grid grid-cols-3 gap-4"
+          value={colors}
+          onValueChange={(newValue) => {
+            changeThemeColors(newValue as typeof colors);
+          }}
+        >
+          {(Object.keys(options) as Array<TTheme["colors"]>).map((option) => (
+            <ColorItem key={option} value={option} option={options[option]} />
+          ))}
+        </RadioGroup.Root>
       </div>
-    </>
+      <div className="mb-5">
+        <p className="text-primary-10 mb-4">Modo</p>
+        <RadioGroup.Root
+          value={mode}
+          onValueChange={(newValue) => {
+            changeThemeMode(newValue as typeof mode);
+          }}
+          className="flex"
+        >
+          <ModeItem value="light">
+            <Sun className="w-4 h-4" /> Claro
+          </ModeItem>
+          <ModeItem value="dark">
+            <Moon className="w-4 h-4" />
+            Oscuro
+          </ModeItem>
+        </RadioGroup.Root>
+      </div>
+    </div>
   );
 }
