e4a322c180c4fea43d35645a24f6c5fbfb671c7f
jeantivan <jptivan53@gmail.com>
2023-08-15 01:19:19 -0400

    CHANGE: Usa el comp ScrollArea. Reemplazado AttachFiles con AddImage. Se pasa el chatId a SendMessage. Cambios en los estilos

--- src/components/Chat/Editor/index.tsx
+++ src/components/Chat/Editor/index.tsx
@@ -7,15 +7,10 @@ import { Laugh } from "lucide-react";
 import { Root } from "./Root";
 import { Plugins } from "./Plugins";
 import { SendMessage } from "./SendMessage";
-import { AttachFiles } from "./AttachFiles";
+import { AddImage } from "./AddImage";
 
-import { Icon } from "@/components/ui/Icon";
-
-const Placeholder = () => (
-  <div className="absolute pl-1 pointer-events-none text-background-11">
-    Escribe un mensaje aquí
-  </div>
-);
+import { IconButton } from "@/components/ui/IconButton";
+import { ScrollArea } from "@/components/ui/ScrollArea";
 
 type EditorProps = {
   chatId: string;
@@ -23,26 +18,33 @@ type EditorProps = {
 export function Editor({ chatId }: EditorProps) {
   return (
     <Root>
-      <AttachFiles />
-      <div className="flex-1 relative min-h-[44px] flex overflow-y-auto bg-background-5 rounded-md">
-        <div className="flex items-end w-full max-h-[136px] py-2 max-w-full px-3">
+      <AddImage />
+      <ScrollArea
+        type="auto"
+        className="max-h-[136px]"
+        rootClassName="flex-1 w-full relative bg-background-5 rounded-md py-1.5"
+      >
+        <div className="flex gap-1 items-end px-3">
           <RichTextPlugin
             contentEditable={
-              <ContentEditable className="w-full bg-transparent focus:outline-none dark:text-white text-black" />
+              <ContentEditable className="w-full h-full bg-transparent focus:outline-none dark:text-white text-black" />
             }
-            placeholder={<Placeholder />}
+            placeholder={
+              <div className="absolute top-2 pl-1 pointer-events-none text-background-11">
+                Escribe un mensaje aquí
+              </div>
+            }
             ErrorBoundary={LexicalErrorBoundary}
           />
-          <button className="w-7 h-7 inline-flex rounded-full text-background-10">
-            <Icon
-              icon={Laugh}
-              label="Insertar emojis"
-              className="w-full h-full"
-            />
-          </button>
+          <IconButton
+            icon={Laugh}
+            label="Insertar emojis"
+            className="w-7 h-7 p-0 text-background-10"
+          />
         </div>
-      </div>
-      <SendMessage />
+      </ScrollArea>
+
+      <SendMessage chatId={chatId} />
       <Plugins />
     </Root>
   );
