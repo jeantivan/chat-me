f3422ba7451b248dc84b49d226d0884085056a02
jeantivan <jptivan53@gmail.com>
2023-08-15 00:23:52 -0400

    CHANGE: Se usa un IconButton. Función createMessage definida fuera del componente. Se recibe el id del chat como prop

--- src/components/Chat/Editor/SendMessage.tsx
+++ src/components/Chat/Editor/SendMessage.tsx
@@ -1,61 +1,65 @@
+import dayjs from "dayjs";
 import { v4 as uuid } from "uuid";
 import { SendIcon } from "lucide-react";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { useLexicalIsTextContentEmpty } from "@lexical/react/useLexicalIsTextContentEmpty";
-import { Icon } from "@/components/ui/Icon";
-import useStore from "@/lib/store";
+
+import { IconButton } from "@/components/ui/IconButton";
 import { SAVE_EDITOR } from "@/plugins/SaveEditorPlugin";
 import { TMessage } from "@/lib/types";
 import mc from "@/lib/utils/mergeClassnames";
-import { useCurrentChatId, useUserId } from "@/lib/hooks";
-import dayjs from "dayjs";
+import useStore from "@/lib/store";
+import { useUserId } from "@/lib/hooks";
 
-export function SendMessage() {
+const createMessage = ({
+  chatId,
+  owner,
+  body = "",
+}: {
+  chatId: string;
+  owner: string;
+  body?: string;
+}): TMessage => ({
+  id: uuid(),
+  chatId,
+  owner,
+  body,
+  starred: false,
+  forwarded: false,
+  hasMedia: null,
+  status: "read",
+  reactions: [],
+  time: dayjs().toISOString(),
+});
+
+export function SendMessage({ chatId }: { chatId: string }) {
   const [editor] = useLexicalComposerContext();
   const isEditorEmpty = useLexicalIsTextContentEmpty(editor, true);
-  const { currentChatId } = useCurrentChatId();
   const userId = useUserId();
   const addMessage = useStore((state) => state.addMessage);
 
-  const createMessage = (body: string): TMessage => {
-    return {
-      id: uuid(),
-      chatId: currentChatId,
-      owner: userId,
-      starred: false,
-      forwarded: false,
-      hasMedia: null,
-      status: "read",
-      reactions: [],
-      time: dayjs().toISOString(),
-      body,
-    };
-  };
-
   const handleSubmit = () => {
-    if (!currentChatId) return;
+    if (isEditorEmpty) return;
 
     editor.dispatchCommand(SAVE_EDITOR, {
-      onSave: (body: string) => addMessage(createMessage(body)),
+      onSave: (body: string) =>
+        addMessage(createMessage({ body, owner: userId, chatId })),
       asHtml: true,
     });
   };
   return (
-    <button
+    <IconButton
+      icon={SendIcon}
+      label="Enviar mensaje"
+      iconClassName="rotate-45"
       className={mc(
-        "w-11 h-11 p-2 rounded-md inline-flex items-center justify-center",
-        "bg-primary-9 text-primary-12",
-        "disabled:bg-primary-5 disabled:text-background-10"
+        "w-10 h-10",
+        "bg-primary-9 hover:bg-primary-10 text-primary-12",
+        "disabled:bg-primary-5 disabled:text-background-10",
+        "[&_span]:-ml-1"
       )}
       onClick={handleSubmit}
       disabled={isEditorEmpty}
-    >
-      <Icon
-        icon={SendIcon}
-        label="Enviar mensaje"
-        className="-ml-1.5"
-        iconClassName="rotate-45"
-      />
-    </button>
+    />
   );
 }
