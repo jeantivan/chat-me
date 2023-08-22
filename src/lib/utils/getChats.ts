69d0f6d4ffbcccaedd297cad50659299826501d6
jeantivan <jptivan53@gmail.com>
2023-08-22 00:09:50 -0400

    CHANGE: Todos los contactos tienen un chat asignado, este chat puede estar vacio o no

--- src/lib/utils/getChats.ts
+++ src/lib/utils/getChats.ts
@@ -3,27 +3,38 @@ import { v4 as uuid } from "uuid";
 import { TChat, TUser } from "@/lib/types";
 import { mapMessages } from "./mapMessages";
 
-const allMessages = await import("../../assets/mock-data/messages.json").then(
-  (res) => res.default
-);
-
 export const getChats = (contacts: TUser[]) => {
-  const randomChats = shuffle(contacts).slice(0, 20);
+  const allChats = shuffle(contacts);
+  const randomPinnedChat = Math.floor(Math.random() * 20);
 
-  const randomPinnedChat = Math.floor(Math.random() * randomChats.length);
+  const chatsWithMessages: TChat[] = allChats
+    .slice(0, 20)
+    .map((contact, index) => {
+      const id = uuid();
 
-  const chats: TChat[] = randomChats.map((contact, index) => {
+      return {
+        id,
+        participants: [contact],
+        pinned: randomPinnedChat === index,
+        muted: false,
+        archived: 35 % index === 0 || 25 % index === 0,
+        hasUnreadMsg: 0,
+        messages: mapMessages(contact, id),
+      };
+    });
+
+  const emptyChats: TChat[] = allChats.slice(21).map((contact) => {
     const id = uuid();
     return {
       id,
       participants: [contact],
-      pinned: randomPinnedChat === index,
+      pinned: false,
       muted: false,
-      archived: 35 % index === 0 || 25 % index === 0,
+      archived: false,
       hasUnreadMsg: 0,
-      messages: mapMessages(allMessages, contact, id),
+      messages: [],
     };
   });
 
-  return chats;
+  return [...chatsWithMessages, ...emptyChats];
 };
