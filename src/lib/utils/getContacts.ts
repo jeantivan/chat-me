b3e16f6d71c84def3f560d0d169b474157853e4c
jeantivan <jptivan53@gmail.com>
2023-08-22 00:03:53 -0400

    ADD: Retorna los contactos ordenados por la primera letra del nombre

--- src/lib/utils/getContacts.ts
+++ src/lib/utils/getContacts.ts
@@ -1,7 +1,8 @@
-import { TUser } from "../types";
+import orderBy from "lodash/orderBy";
+import { TUser } from "@/lib/types";
 
 export const getContacts = async (): Promise<TUser[]> => {
-  return await import("../../assets/mock-data/contacts.json").then(
-    (res) => res.default
+  return await import("../../assets/mock-data/contacts.json").then((res) =>
+    orderBy(res.default, "name[0]")
   );
 };
