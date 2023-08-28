d06060751e96f424cc4d4b682df00f373005d5e3
jeantivan <jptivan53@gmail.com>
2023-08-28 18:57:36 -0400

    ADD: Mas clases al tema de Lexical

--- src/components/Chat/Editor/theme.ts
+++ src/components/Chat/Editor/theme.ts
@@ -1,3 +1,6 @@
 export const theme = {
+  paragraph: "whitespace-pre-wrap break-words",
   link: "text-blue-600 font-medium",
+  emoji:
+    "emoji w-5 h-5 inline-block align-top text-transparent bg-cover select-text"
 };
