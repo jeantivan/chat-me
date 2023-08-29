2acd5b4c3f6f4a2f51c62b8e2a3f8f4acf7cddc8
jeantivan <jptivan53@gmail.com>
2023-08-29 19:37:33 -0400

    CHANGE: Vertical align es text-top

--- src/components/Chat/Editor/theme.ts
+++ src/components/Chat/Editor/theme.ts
@@ -2,5 +2,5 @@ export const theme = {
   paragraph: "whitespace-pre-wrap break-words",
   link: "text-blue-600 font-medium",
   emoji:
-    "emoji w-5 h-5 inline-block align-top text-transparent bg-cover select-text"
+    "emoji w-5 h-5 inline-block align-text-top text-transparent bg-cover select-text"
 };
