c86dc4a3ebd3948855284a09398e8ccd7ad8dac4
jeantivan <jptivan53@gmail.com>
2023-08-30 00:18:00 -0400

    ADD: Se usa lexical para parsear el body del mensaje

--- src/components/Message/MessageText.tsx
+++ src/components/Message/MessageText.tsx
@@ -1,9 +1,57 @@
-export function MessageText({ content }: { content: string }) {
+import { ReactNode } from "react";
+
+import {
+  LexicalComposer,
+  InitialConfigType
+} from "@lexical/react/LexicalComposer";
+
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+import { baseConfig } from "@/editor/baseConfig";
+
+import mc from "@/lib/utils/mergeClassnames";
+
+type RootProps = {
+  children: ReactNode;
+  initialState: string;
+};
+function Root({ children, initialState }: RootProps) {
+  const editorConfig: InitialConfigType = {
+    ...baseConfig,
+    namespace: "MessageParser",
+    editable: false,
+    editorState: initialState
+  };
   return (
-    <div className="py-1 px-2">
-      <div className="whitespace-pre-wrap break-all leading-tight">
-        {content}
+    <LexicalComposer initialConfig={editorConfig}>
+      <>{children}</>
+    </LexicalComposer>
+  );
+}
+
+type Props = {
+  body: string;
+  className?: string;
+};
+export function MessageText({ body, className }: Props) {
+  return (
+    <Root initialState={body}>
+      <div className="py-1 px-2">
+        <RichTextPlugin
+          contentEditable={
+            <ContentEditable
+              className={mc(
+                "whitespace-pre-wrap break-words leading-tight",
+                className
+              )}
+            />
+          }
+          placeholder={null}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
       </div>
-    </div>
+    </Root>
   );
 }
