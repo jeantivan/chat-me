6565e7325df9e2f7cb6b388be4cfa80499d7a12d
jeantivan <jptivan53@gmail.com>
2023-08-29 21:09:49 -0400

    ADD: Envuelve el texto del  mensaje en un Lexical Editor

--- src/lib/utils/mapMessages.ts
+++ src/lib/utils/mapMessages.ts
@@ -6,6 +6,9 @@ const allMessages = (await import("../../assets/mock-data/messages.json").then(
   (res) => res.default
 )) as TMessage[];
 
+const wrapInLexicalEditor = (body: string) =>
+  `{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"${body}","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}`;
+
 export const mapMessages = (contact: TUser, chatId: string): TMessage[] => {
   const qty = Math.floor(Math.random() * 40 + 1);
   const messages = shuffle(allMessages)
@@ -17,11 +20,14 @@ export const mapMessages = (contact: TUser, chatId: string): TMessage[] => {
 
       const owner = message.owner === null ? contact.id : message.owner;
 
+      const body = wrapInLexicalEditor(message.body);
+
       return {
         ...message,
         chatId,
         time: newTime,
         owner,
+        body
       };
     });
 
