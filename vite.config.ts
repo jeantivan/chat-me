2dddb8e1e64f2b389e8b8dca6d0a6e7b6fa03528
jeantivan <jptivan53@gmail.com>
2023-08-30 00:23:36 -0400

    CHANGE: No se abre una nueva pestaña al iniciar el servidor

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
