5d154638e8e5075f19292f292dd208fbc8e6a5d9
jeantivan <jptivan53@gmail.com>
2023-09-05 20:33:33 -0400

    CHANGE: Simplificada la estructura del componente

--- src/components/Chat/Editor/index.tsx
+++ src/components/Chat/Editor/index.tsx
@@ -1,13 +1,8 @@
-import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
-import { ContentEditable } from "@lexical/react/LexicalContentEditable";
-import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
-
 import { Root } from "./Root";
 import { SendMessage } from "./SendMessage";
-import { AddImage } from "./AddImage";
 
-import { ScrollArea } from "@/components/ui/ScrollArea";
 import { EmojiPicker } from "./EmojiPicker";
+import { Input } from "./Input";
 
 type EditorProps = {
   chatId: string;
@@ -15,27 +10,10 @@ type EditorProps = {
 export function Editor({ chatId }: EditorProps) {
   return (
     <Root chatId={chatId}>
-      <AddImage />
-      <ScrollArea
-        type="auto"
-        className="max-h-[136px]"
-        rootClassName="flex-1 w-full relative bg-background-5 rounded-md py-1.5"
-      >
-        <div className="flex gap-1 items-end px-3">
-          <RichTextPlugin
-            contentEditable={
-              <ContentEditable className="w-full h-full bg-transparent focus:outline-none dark:text-white text-black" />
-            }
-            placeholder={
-              <div className="absolute top-2 pl-1 pointer-events-none text-background-11">
-                Escribe un mensaje aquí
-              </div>
-            }
-            ErrorBoundary={LexicalErrorBoundary}
-          />
-          <EmojiPicker />
-        </div>
-      </ScrollArea>
+      <div className="flex items-start gap-2.5 pl-2.5 bg-background-5 rounded-md w-full py-1.5">
+        <EmojiPicker />
+        <Input />
+      </div>
       <SendMessage />
     </Root>
   );
